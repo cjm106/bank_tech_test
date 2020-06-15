02d666b0211c9c183504e13044a705b3d2470db2
cjm106 <3774946+cjm106@users.noreply.github.com>
2020-06-15 12:22:47 +0100

    Banking class can now accept deposits

--- lib/banking.rb
+++ lib/banking.rb
@@ -1,5 +1,18 @@
 
 class Banking
 
+  attr_reader :deposits
+
+  def initialize
+    @deposits = []
+  end
+
+  def add(amount)
+    date = Time.now.strftime("%d/%m/%Y")
+
+    @deposits << [amount, date]
+
+  end
+
 
 end
--- spec/banking_spec.rb
+++ spec/banking_spec.rb
@@ -2,4 +2,13 @@ require './lib/banking'
 
 describe Banking do
 
+  subject = Banking.new
+
+  it "is able to accept deposits" do
+    current_date = Time.now.strftime("%d/%m/%Y")
+    subject.add(1000)
+    expect(subject.deposits).to eq([[1000, current_date]])
+  end
+
+
 end
